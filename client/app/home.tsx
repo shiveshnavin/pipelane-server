0715c52a4413ddc8bd8fca6e4b9ae9a537d99a6f
Shivesh Navin <shiveshnavin@gmail.com>
2024-04-11 14:36:05 +0530

    Add data list

--- client/app/home.tsx
+++ client/app/home.tsx
@@ -2,32 +2,33 @@ import { AppContext } from "@/components/Context";
 import { gql } from "@apollo/client";
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
-import { Avatar, Center, PressableView, Subtitle, TextView, ThemeContext, VBox, VPage } from "react-native-boxes";
-import { User } from '../../gen/model'
+import { Avatar, Center, Icon, PressableView, SimpleDatalistView, Subtitle, SwitchView, TextView, ThemeContext, VBox, VPage } from "react-native-boxes";
+import { Pipelane } from '../../gen/model'
 import { router } from "expo-router";
-import { ScrollView } from "react-native";
+import { ScrollView, Switch } from "react-native";
 import KeyboardAvoidingScrollView, { HBox } from "react-native-boxes/src/Box";
 
 export default function HomeLayout() {
     const theme = useContext(ThemeContext)
     const appContext = useContext(AppContext)
     const graph = appContext.context.api.graph
-    const [users, setUsers] = useState([])
+    const [pipes, setUsers] = useState([])
     useEffect(() => {
         let query = `
-        query GetUsers {
-            users {
-                id
-                first_name
-                userName
-
+        query GetPipes {
+            pipelanes {
+              name
+              inputs
+              schedule
+              active
             }
-        }
+          }
+          
      `
         graph.query({
             query: gql(query),
         }).then((result: any) => {
-            setUsers(result.data.users)
+            setUsers(result.data.pipelanes)
         });
     }, [])
     return (
@@ -37,40 +38,34 @@ export default function HomeLayout() {
                 <KeyboardAvoidingScrollView style={{
                     width: '100%'
                 }}>
-                    {
-                        users.map((u: User) => {
-                            return (
-                                <PressableView
-                                    key={u.id}
-                                    onPress={() => {
-                                        router.navigate(`/search/${u.id}`)
-                                    }}
-                                    style={{
-                                        width: '100%'
-                                    }}>
-                                    <VBox style={{
-                                        margin: theme.dimens.space.md,
-                                        width: '98%',
-                                        padding: theme.dimens.space.md,
-                                        backgroundColor: theme.colors.forground,
-                                        borderRadius: theme.dimens.space.md
-                                    }}>
-                                        <HBox style={{
-                                            alignItems: 'center'
-                                        }}>
-                                            <Avatar iconText={u.first_name?.substring(0, 2).toUpperCase()} />
-                                            <VBox>
-                                                <Subtitle style={{
-                                                    fontFamily: theme.fonts.Bold
-                                                }}>{u.userName}</Subtitle>
-                                                <TextView>{u.first_name}</TextView>
-                                            </VBox>
-                                        </HBox>
-                                    </VBox>
-                                </PressableView>
-                            )
-                        })
-                    }
+
+                    <SimpleDatalistView
+                        style={{
+                            padding: theme.dimens.space.sm
+                        }}
+                        items={pipes}
+                        //@ts-ignore
+                        itemAdapter={(pipe: Pipelane, idx: number) => {
+                            const [isOn, setIsOn] = useState(false)
+                            return {
+                                action: (
+                                    <SwitchView
+                                        value={isOn}
+                                        onValueChange={(p) => {
+                                            console.log('presss', p)
+                                            setIsOn(p)
+                                        }} />
+                                ),
+                                title: pipe.name,
+                                subtitle: pipe.schedule,
+                                icon: (
+                                    <Avatar iconText={pipe.name?.toUpperCase()} />
+                                ),
+                                flexRatio: [3, 7, 1],
+                                body: pipe.inputs
+                            }
+                        }} />
+
                 </KeyboardAvoidingScrollView>
             </Center>
         </VPage>
