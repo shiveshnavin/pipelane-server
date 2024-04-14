a4c16de95dfe347fdcdb7a9cfe4b40483fe784a4
Shivesh Navin <shiveshnavin@gmail.com>
2024-04-14 22:12:52 +0530

    Checkpoint

--- server/graphql/pipelane.ts
+++ server/graphql/pipelane.ts
@@ -11,7 +11,7 @@ export function generatePipelaneResolvers(
     cronScheduler?: CronScheduler) {
     const PipelaneResolvers = {
         Pipelane: {
-            nextRun: (parent: Pipelane) => cronScheduler.getNextRun(parent.schedule).toLocaleString('Asia/Kolkata'),
+            nextRun: (parent: Pipelane) => cronScheduler.getNextRun(parent.schedule).toLocaleString('en-IN'),
             tasks: async (parent) => {
                 if (parent.tasks) return parent.tasks
                 let tasks = await db.get(TableName.PS_PIPELANE_TASK,
