ae8e9457c7efb8e995c97f5cd99137e8fcc205aa
Shivesh Navin <shiveshnavin@gmail.com>
2024-04-14 13:08:16 +0530

    Added task ui

--- client/app/home/[pipe]/[task]/index.tsx
+++ client/app/home/[pipe]/[task]/index.tsx
@@ -4,26 +4,48 @@ import { useLocalSearchParams } from "expo-router";
 import { useRouteInfo, useRouter } from "expo-router/build/hooks";
 import React, { useEffect, useReducer, useState } from "react";
 import { useContext } from "react";
-import { TransparentCenterToolbar, Center, Expand, SimpleToolbar, TextView, ThemeContext, Title, VBox, VPage, CardView, CompositeTextInputView, SwitchView, HBox, SimpleDatalistView, SimpleDatatlistViewItem, Icon, Subtitle } from "react-native-boxes";
+import { TransparentCenterToolbar, Center, Expand, SimpleToolbar, TextView, ThemeContext, Title, VBox, VPage, CardView, CompositeTextInputView, SwitchView, HBox, SimpleDatalistView, SimpleDatatlistViewItem, Icon, Subtitle, DropDownView } from "react-native-boxes";
 import { AlertMessage, Spinner } from "react-native-boxes";
-import { Maybe, Pipelane, Pipetask, PipetaskExecution } from "../../../../../gen/model";
+import { Maybe, Pipelane, Pipetask, PipetaskExecution, TaskType } from "../../../../../gen/model";
 import { getGraphErrorMessage } from "@/common/api";
 
 export default function PipeTaskPage() {
     const theme = useContext(ThemeContext)
     const { pipe: pipelaneName, task: taskVariantName } = useLocalSearchParams();
     const [curPipetask, setCurPipetask] = useState<Pipetask | undefined>(undefined)
+    const [taskTypes, setTaskTypes] = useState([])
     const [err, seterr] = useState<undefined | string>(undefined)
     const [loading, setLoading] = useState(true)
     const appContext = useContext(AppContext)
     const api = appContext.context.api
 
-    function getPipetask(getTasks?: boolean) {
+    function getPipetask() {
 
         setLoading(true)
         seterr(undefined)
-        api.getPipetask(pipelaneName as string, taskVariantName as string).then(result => {
+
+        api.graph.query({
+            query: gql`query PipeTasks($taskVariantName: ID!, $pipelaneName: ID!) {
+                Pipetask(taskVariantName: $taskVariantName, pipelaneName: $pipelaneName) {
+                  pipelaneName
+                  taskVariantName
+                  taskTypeName
+                  isParallel
+                  input
+                }
+                taskTypes {
+                  type
+                  variants
+                }
+              }
+              `,
+            variables: {
+                pipelaneName,
+                taskVariantName
+            }
+        }).then(result => {
             setCurPipetask(result.data.Pipetask)
+            setTaskTypes(result.data.taskTypes)
             if (!result.data.Pipetask) {
                 seterr(`No task exists with name ${taskVariantName} in ${pipelaneName}`)
             }
@@ -51,13 +73,13 @@ export default function PipeTaskPage() {
                 }} />
             }
             {
-                curPipetask && <PipetaskView pipetask={curPipetask} />
+                curPipetask && <PipetaskView taskTypes={taskTypes} pipetask={curPipetask} />
             }
         </VPage>
     );
 }
 
-function PipetaskView({ pipetask: inputPipetask }: { pipetask: Pipetask }) {
+function PipetaskView({ pipetask: inputPipetask, taskTypes }: { pipetask: Pipetask, taskTypes: TaskType[] }) {
     const router = useRouter()
     const [task, setTask] = useState<Pipetask>(
         {
@@ -92,15 +114,33 @@ function PipetaskView({ pipetask: inputPipetask }: { pipetask: Pipetask }) {
                         }}
                     />
                 </HBox>
-                <CompositeTextInputView
-                    placeholder="Task variant name"
-                    value={task.taskVariantName as string}
-                    onChangeText={(nt) => {
-                        task.taskVariantName = nt
+                <DropDownView
+                    title="Task Type"
+                    forceDialogSelectOnWeb={true}
+                    onSelect={(taskTypeName) => {
+                        task.taskTypeName = taskTypeName
                         forceUpdate()
                     }}
-                />
-
+                    selectedId={task.taskTypeName}
+                    options={taskTypes.map(tt => ({
+                        id: tt.type,
+                        value: tt.type,
+                        title: tt.type,
+                    }))} />
+                <DropDownView
+                    title="Task Variant"
+                    forceDialogSelectOnWeb={true}
+                    onSelect={(taskVariantName) => {
+                        task.taskVariantName = taskVariantName
+                        forceUpdate()
+                    }}
+                    selectedId={task.taskVariantName}
+                    //@ts-ignore
+                    options={taskTypes?.find(t => t.type == task.taskTypeName)?.variants?.map(tt => ({
+                        id: tt,
+                        value: tt,
+                        title: tt,
+                    }) || [])} />
                 <CompositeTextInputView
                     icon="close"
                     placeholder="Inputs"
--- server/graphql/resolvers.ts
+++ server/graphql/resolvers.ts
@@ -2,16 +2,16 @@ import { MultiDbORM } from "multi-db-orm"
 import { TaskVariantConfig } from "pipelane"
 import { generateTaskTypeResolvers } from "./tasktypes"
 import { generatePipelaneResolvers } from "./pipelane"
-
+import _ from 'lodash'
 export function generateResolvers(db: MultiDbORM, variantConfig: TaskVariantConfig) {
     const resolvers = [
         generateTaskTypeResolvers(variantConfig),
         generatePipelaneResolvers(db, variantConfig)
     ];
 
-    const resolver = {}
+    let resolver = {}
     resolvers.forEach(r => {
-        Object.assign(resolver, r)
+        resolver = _.defaultsDeep(resolver, r)
     })
 
     return resolver
