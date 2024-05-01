9b9280c5f6e5dc6a3e5a4fb77794e85313eb2072
Shivesh Navin <shiveshnavin@gmail.com>
2024-05-01 14:16:56 +0530

    Add shell exec task

---
+++ server/pipe-tasks/ShellTask.ts
@@ -0,0 +1,71 @@
+import axios from "axios";
+import PipeLane, { PipeTask } from "pipelane";
+import { exec } from 'child_process'
+
+export type ShellTaskAdditionalInput = {
+    cmd: string
+}
+
+export class ShellTask extends PipeTask<any, any> {
+
+    static TASK_VARIANT_NAME: string = "shell"
+    static TASK_TYPE_NAME: string = "shell"
+
+    allowedCommands = []
+
+    constructor(variantName?: string, allowedCommands?: string[]) {
+        super(ShellTask.TASK_TYPE_NAME, variantName || ShellTask.TASK_VARIANT_NAME)
+        this.allowedCommands = allowedCommands || []
+    }
+
+    kill(): boolean {
+        return true
+    }
+
+
+    isExecutableAllowed(command, allowedExecutables) {
+        const commands = command.split(/[&;&|&&|]/);
+        // Iterate through each command
+        for (let i = 0; i < commands.length; i++) {
+            const singleCommand = commands[i].trim().split(' ')[0];
+            if (!allowedExecutables.includes(singleCommand)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    async execute(pipeWorkInstance: PipeLane, input: { inputs: any[], additionalInputs: ShellTaskAdditionalInput }): Promise<any[]> {
+
+        let cmd = input.additionalInputs.cmd
+
+        if (!this.isExecutableAllowed(cmd, this.allowedCommands)) {
+            return [{
+                status: false,
+                message: 'Command not allowed'
+            }]
+        }
+
+        return new Promise((resolve, reject) => {
+
+            console.log('Executing command', cmd)
+            exec(cmd,
+                (error, stdout, stderr) => {
+                    if (error !== null) {
+                        resolve([{
+                            status: false,
+                            message: 'Error in exec: ' + error
+                        }])
+                    } else {
+                        resolve([{
+                            status: true,
+                            output: stdout
+                        }])
+                    }
+                });
+
+        })
+    }
+
+}
--- server/pipe-tasks/index.ts
+++ server/pipe-tasks/index.ts
@@ -4,3 +4,5 @@ import { ApiTask } from "./ApiTask";
 export const VariantConfig: TaskVariantConfig = {
     [ApiTask.TASK_TYPE_NAME]: [new ApiTask('api-call-a'), new ApiTask('api-call-b')]
 }
+
+export * from './'
