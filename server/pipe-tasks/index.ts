1e9a6daa78fc6f501ebf149a48681231ae1b5236
shiveshnavin <shiveshnavin@gmail.com>
2024-06-19 22:47:20 +0530

    Update variant config

--- server/pipe-tasks/index.ts
+++ server/pipe-tasks/index.ts
@@ -1,8 +1,14 @@
 import { TaskVariantConfig } from "pipelane";
 import { ApiTask } from "./ApiTask";
+import { ShellTask } from "./ShellTask";
+import { DelayTask } from "./DelayTask";
+import { EvaluateJsTask } from "./EvaluateJsTask";
 
 export const VariantConfig: TaskVariantConfig = {
-    [ApiTask.TASK_TYPE_NAME]: [new ApiTask('api-call-a'), new ApiTask('api-call-b')]
+    [ApiTask.TASK_TYPE_NAME]: [new ApiTask(ApiTask.TASK_VARIANT_NAME)],
+    [ShellTask.TASK_TYPE_NAME]: [new ShellTask(ShellTask.TASK_VARIANT_NAME)],
+    [DelayTask.TASK_TYPE_NAME]: [new DelayTask(DelayTask.TASK_VARIANT_NAME)],
+    [EvaluateJsTask.TASK_TYPE_NAME]: [new EvaluateJsTask(EvaluateJsTask.TASK_VARIANT_NAME)],
 }
 
 export * from './'
