0236517dbbbcd22b4b0896e41b88b3627201a3de
shiveshnavin <shiveshnavin@gmail.com>
2024-10-11 15:26:22 +0530

    Add pipe manager

--- server/pipe-tasks/PipeManagerTask.ts
+++ server/pipe-tasks/PipeManagerTask.ts
@@ -4,12 +4,15 @@ import { method } from "lodash";
 import PipeLane, { PipeTask } from "pipelane";
 
 
-/**
- * input : { 
- *      last:[{ pipeName , action: enable | disable. access_token }], 
- *      additionalInputs: { pipeName , action: enable | disable, access_token } 
- * }
- */
+export type PipeManagerInput = {
+    pipeName: string,
+    action: "enable" | "disable" | "delete" | "trigger",
+    access_token: string,
+    status: boolean
+    trigger_inputs?: any
+    endpoint: string
+}
+
 export class PipeManagerTask extends PipeTask<any, any> {
 
     static TASK_VARIANT_NAME: string = "manager"
@@ -23,14 +26,56 @@ export class PipeManagerTask extends PipeTask<any, any> {
         return true
     }
 
+
+    getConfig(pipe: PipeManagerInput) {
+        let req: any = {
+            method: 'post',
+            url: pipe.endpoint,
+            headers: {
+                'Authorization': `Bearer ${pipe.access_token}`
+            },
+            data: {
+                "operationName": "Mutation",
+                "variables":
+                {
+                    "data": {
+                        "active": pipe.action == 'enable',
+                        "name": pipe.pipeName
+                    }
+                },
+                "query": `mutation Mutation($data: CreatePipelanePayload!, $oldPipeName: ID) {\n  createPipelane(data: $data, oldPipeName: $oldPipeName) {\n    
+                        name\n    
+                        active\n    
+                        schedule\n    
+                        input\n    
+                        nextRun\n    
+                        retryCount\n    
+                        executionsRetentionCount\n    
+                        updatedTimestamp\n    
+                        __typename\n  
+                }\n
+                }`
+            }
+        }
+
+        if (pipe.action == 'delete') {
+            req.data = {
+                "operationName": "DeletePipelane",
+                "variables": { "name": pipe.pipeName },
+                "query": "mutation DeletePipelane($name: ID!) {\n  deletePipelane(name: $name)\n}"
+            }
+        }
+        if (pipe.action == 'trigger') {
+            req.data = {
+                "operationName": "executePipelane",
+                "variables": { "name": pipe.pipeName, "input": JSON.stringify(pipe.trigger_inputs || {}) }, "query": "mutation executePipelane($name: ID!, $input: String!) {\n  executePipelane(name: $name, input: $input) {\n    id\n    __typename\n  }\n}"
+            }
+        }
+    }
+
     async execute(pipeWorkInstance: PipeLane, input: any): Promise<any[]> {
 
-        let pipeManagerInput: {
-            pipeName: string,
-            action: "enable" | "disable",
-            access_token: string,
-            status: boolean
-        }[] = []
+        let pipeManagerInput: PipeManagerInput[] = []
         if (input.last && input.last.length > 0) {
             pipeManagerInput.push(...input.last)
         }
@@ -44,35 +89,7 @@ export class PipeManagerTask extends PipeTask<any, any> {
                 if (!pipe.action) {
                     pipe.action = 'enable'
                 }
-                let request = {
-                    method: 'post',
-                    url: "https://stocks.semibit.in/pipelane/graph",
-                    headers: {
-                        'Authorization': `Bearer ${pipe.access_token}`
-                    },
-                    data: {
-                        "operationName": "Mutation",
-                        "variables":
-                        {
-                            "data": {
-                                "active": pipe.action == 'enable',
-                                "name": pipe.pipeName
-                            }
-                        },
-                        "query": `mutation Mutation($data: CreatePipelanePayload!, $oldPipeName: ID) {\n  createPipelane(data: $data, oldPipeName: $oldPipeName) {\n    
-                                name\n    
-                                active\n    
-                                schedule\n    
-                                input\n    
-                                nextRun\n    
-                                retryCount\n    
-                                executionsRetentionCount\n    
-                                updatedTimestamp\n    
-                                __typename\n  
-                        }\n
-                        }`
-                    }
-                }
+                let request: any = this.getConfig(pipe)
                 promises.push(axios(request).then(res => {
                     pipe.status = true
                     return pipe
