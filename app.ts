731c719e8819be8ed20d21cadeedd1ca92157e0f
Shivesh Navin <shiveshnavin@gmail.com>
2025-05-12 21:27:18 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import { creatPipelaneServer } from "./server";
 import { VariantConfig } from "./server/pipe-tasks";
 import { readFileSync } from "fs";
 import path from "path";
+import https from 'https'
 import express from 'express'
 import { createMcpServer } from "./server/mcp";
 
@@ -16,4 +17,18 @@ creatPipelaneServer(VariantConfig, db, 2).then(pipelane => {
     app.listen(port, () => {
         console.log(`Running a GraphQL API server at http://localhost:${port}/graph. Current time: ${new Date().toLocaleString()}`)
     })
+
+    if(process.env.PIPELANE_HTTPS_PORT && process.env.PIPELANE_HTTPS_KEY_PATH && process.env.PIPELANE_HTTPS_CERT_PATH){
+        let HTTPS_PORT = process.env.PIPELANE_HTTPS_PORT || 8443
+        var privateKey = fs.readFileSync(process.env.PIPELANE_HTTPS_KEY_PATH);
+        var certificate = fs.readFileSync(process.env.PIPELANE_HTTPS_CERT_PATH);
+        https
+            .createServer({
+                key: privateKey,
+                cert: certificate
+            }, app)
+            .listen(HTTPS_PORT, () => {
+                console.log('Pipelane https server UP on port ', HTTPS_PORT)
+            });
+    }
 })
